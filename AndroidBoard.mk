89f0533f031633e5d170b7d528d90d0ecbef9615
ZVNexus <zvnexus@outlook.com>
2019-03-02 14:51:53 -0500

    Move keylayout references to keylayout/Android.mk
    This makes more sense.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,22 +4,6 @@ LOCAL_PATH := $(call my-dir)
 # Copy additional target-specific files
 #----------------------------------------------------------------------
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := synaptics.kl
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := keylayout/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fpc1020.kl
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := keylayout/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
-include $(BUILD_PREBUILT)
-
 #Create dsp directory
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/dsp)
 
