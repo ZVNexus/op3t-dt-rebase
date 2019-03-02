07b930f9df02a6c11df8c3f2bf3a879208683340
ZVNexus <zvnexus@outlook.com>
2019-03-02 14:41:01 -0500

    Add initial FPC1020 keylayout

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -52,6 +52,14 @@ LOCAL_SRC_FILES    := keylayout/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fpc1020.kl
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := keylayout/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
 #include $(CLEAR_VARS)
 #LOCAL_MODULE       := init.qcom.modem_links.sh
 #LOCAL_MODULE_TAGS  := optional eng
